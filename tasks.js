368d90584cfaa2a6c0e2b070490b03cec94c1c6f
Elijah Trillionz <60356516+Elijah-trillionz@users.noreply.github.com>
2020-11-09 09:20:32 -1200

    Create tasks.js

---
+++ tasks.js
@@ -0,0 +1,9 @@
+// leave first task as sample
+tasks: [
+ {
+  id: 1, // numbers only
+  task: 'Task', //strings only
+  src: 'John Doe', // strings only
+  srcURL: 'https://johndoe.com', // string only (https:// or http:// compulsory & domain name (.org, .com etc)).
+ },
+]
