344187fcf8e7ff14efbba0d4713940e4c2448f8e
Elijah Trillionz <60356516+Elijah-trillionz@users.noreply.github.com>
2020-11-09 22:46:34 +0100

    JavaScript tasks
    JavaScript tasks only.

---
+++ JavaScript/tasks.js
@@ -0,0 +1,12 @@
+// leave first task as sample
+// strictly JavaScript tasks / exercises / challenge
+// JAVASCRIPT ONLY
+
+tasks: [
+ {
+  id: 1, // numbers only
+  task: 'Task', //strings only
+  src: 'John Doe', // strings only
+  srcURL: 'https://johndoe.com', // string only (https:// or http:// compulsory & domain name (.org, .com etc)).
+ },
+]
